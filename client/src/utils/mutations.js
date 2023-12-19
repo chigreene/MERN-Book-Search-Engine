0563c9aea71f76281c5015a7e6e7b8ab1f5f5e09
Christopher Greene <chigreenwork@gmail.com>
2023-12-19 15:43:03 -0500

    attempts to refactor SavedBooks.jsx to use mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -43,7 +43,7 @@ export const SAVE_BOOK = gql`
   }
 `;
 
-export const DELETE_BOOK = gql`
+export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
       _id
